426bb6cf5342d3968bf0e8492e1c01b601b196d6
Oscar Bergström <oscar@obstrom.com>
2022-04-11 20:44:08 +0200

    Add basic Packer class

---
+++ src/main/java/com/obstrom/binpacker/Packager.java
@@ -0,0 +1,64 @@
+package com.obstrom.binpacker;
+
+import com.github.skjolber.packing.api.Container;
+import com.github.skjolber.packing.api.StackableItem;
+import com.github.skjolber.packing.packer.PackagerException;
+import com.github.skjolber.packing.packer.laff.LargestAreaFitFirstPackager;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BooleanSupplier;
+
+@RequiredArgsConstructor
+public class Packager {
+
+    private final long TIMEOUT_MILLISECONDS = 1000L;
+
+    private final List<Container> containers;
+    private final List<StackableItem> products = new ArrayList<>();
+    private LargestAreaFitFirstPackager packager = null;
+
+    private boolean isInitialized = false;
+    private boolean isFinished = false;
+
+    private List<Container> result = List.of();
+
+    public void init() {
+        if (containers.isEmpty()) throw new PackagerException("No containers, can't initialize!");
+        if (isInitialized)
+            throw new PackagerException("Packager is already initialized. Create a new packager instead.");
+
+        packager = LargestAreaFitFirstPackager.newBuilder()
+                .withContainers(containers)
+                .build();
+
+        isInitialized = true;
+    }
+
+    public void addContainer(Container container) {
+        if (isInitialized)
+            throw new PackagerException("Packager is already initialized with given containers. Create a new packager instead.");
+        containers.add(container);
+    }
+
+    public void addProduct(StackableItem stackableItem) {
+        products.add(stackableItem);
+    }
+
+    public List<Container> pack() {
+        if (!isInitialized) throw new PackagerException("Packager is not initialized. Initialize the packer first.");
+        if (isFinished) return result;
+
+        List<Container> packagerResult = packager.packList(products, 5, getDeadline());
+        result = (packagerResult == null) ? List.of() : packagerResult; // Switches null value for empty list
+
+        isFinished = true;
+        return result;
+    }
+
+    private Long getDeadline() {
+        return System.currentTimeMillis() + TIMEOUT_MILLISECONDS;
+    }
+
+}
---
+++ src/test/java/com/obstrom/binpacker/PackagerTest.java
@@ -0,0 +1,106 @@
+package com.obstrom.binpacker;
+
+import com.github.skjolber.packing.api.Box;
+import com.github.skjolber.packing.api.Container;
+import com.github.skjolber.packing.api.StackableItem;
+import com.github.skjolber.packing.packer.laff.LargestAreaFitFirstPackager;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PackagerTest {
+
+    @Test
+    void testLibraryBasicSetupWorks() {
+        // given
+        Container container = Container.newBuilder()
+                .withDescription("1")
+                .withSize(10, 10, 3)
+                .withEmptyWeight(1)
+                .withMaxLoadWeight(100)
+                .build();
+
+        LargestAreaFitFirstPackager packager = LargestAreaFitFirstPackager.newBuilder()
+                .withContainers(List.of(container))
+                .build();
+
+        List<StackableItem> products = List.of(
+                new StackableItem(Box.newBuilder().withId("foo").withSize(6, 10, 2).withRotate3D().withWeight(25).build(), 1)
+        );
+
+        // when
+        Container match = packager.pack(products);
+
+        // then
+        assertNotNull(match);
+    }
+
+    @Test
+    void testLibraryBasicSetupWorksMultiple() {
+        // given
+        List<Container> containers = List.of(Container.newBuilder()
+                .withDescription("1")
+                .withSize(10, 10, 3)
+                .withEmptyWeight(1)
+                .withMaxLoadWeight(100)
+                .build());
+
+        LargestAreaFitFirstPackager packager = LargestAreaFitFirstPackager.newBuilder()
+                .withContainers(containers)
+                .build();
+
+        List<StackableItem> products = List.of(
+                new StackableItem(Box.newBuilder().withId("foo").withSize(6, 10, 2).withRotate3D().withWeight(25).build(), 1)
+        );
+
+        // when
+        List<Container> matches = packager.packList(products, 1);
+
+        // then
+        assertNotNull(matches);
+    }
+
+
+    @Test
+    void givenValidBasicContainersAndProducts_whenPackagerPacks_thenFindCorrectContainer() {
+        // given
+        List<Container> containers = List.of(
+                Container.newBuilder()
+                        .withDescription("Test container 1")
+                        .withSize(50, 50, 50)
+                        .withEmptyWeight(1)
+                        .withMaxLoadWeight(999)
+                        .build(),
+                Container.newBuilder()
+                        .withDescription("Test container 2")
+                        .withSize(60, 60, 60)
+                        .withEmptyWeight(1)
+                        .withMaxLoadWeight(999)
+                        .build()
+        );
+        StackableItem stackableItem = new StackableItem(
+                Box.newBuilder()
+                        .withDescription("Test box variation 1")
+                        .withId("test1")
+                        .withSize(50, 50, 50)
+                        .withRotate3D()
+                        .withWeight(1)
+                        .build(),
+                1
+        );
+
+        // when
+        Packager packager = new Packager(containers);
+        packager.init();
+        packager.addProduct(stackableItem);
+        List<Container> result = packager.pack();
+
+        // then
+        assertFalse(result.isEmpty());
+        assertEquals(result.size(), 1);
+        assertEquals(result.get(0).getDescription(), "Test container 1");
+    }
+
+}
