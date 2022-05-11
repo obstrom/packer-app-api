d896da3db0f3d52227fe2cb2227b0f42b2da7904
Oscar Bergström <oscar@obstrom.com>
2022-05-11 22:52:36 +0200

    Added CORS support

--- src/main/java/com/obstrom/packerservice/controller/PackageServiceController.java
+++ src/main/java/com/obstrom/packerservice/controller/PackageServiceController.java
@@ -5,14 +5,12 @@ import com.obstrom.packerservice.dto.PackingJobResponseDto;
 import com.obstrom.packerservice.service.PackingService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @Slf4j
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/packer")
 @AllArgsConstructor
