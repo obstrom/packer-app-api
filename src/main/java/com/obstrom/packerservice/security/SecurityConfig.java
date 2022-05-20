135b3436994e0dbef33433adb3a9012e6365bdc8
Oscar Bergström <oscar@obstrom.com>
2022-05-21 01:47:00 +0200

    Undo JDK change

--- src/main/java/com/obstrom/packerservice/security/SecurityConfig.java
+++ src/main/java/com/obstrom/packerservice/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.obstrom.packerservice.security;
 
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
