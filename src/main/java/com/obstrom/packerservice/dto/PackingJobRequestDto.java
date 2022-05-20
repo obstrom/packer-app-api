ff63b7ee271690f588083b6090b62510d3702184
Oscar Bergström <oscar@obstrom.com>
2022-05-21 01:50:08 +0200

    Back to JDK 17

--- src/main/java/com/obstrom/packerservice/dto/PackingJobRequestDto.java
+++ src/main/java/com/obstrom/packerservice/dto/PackingJobRequestDto.java
@@ -1,17 +1,13 @@
 package com.obstrom.packerservice.dto;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.obstrom.packerservice.units.StandardUnitsUtil;
-import com.obstrom.packerservice.deserializer.PackingJobRequestDtoDeserializer;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
-@JsonDeserialize(using = PackingJobRequestDtoDeserializer.class)
 public record PackingJobRequestDto(
-        @NotNull StandardUnitsUtil.Length lengthUnitType,
-        @NotNull StandardUnitsUtil.Weight weightUnitType,
-        @NotNull @Valid List<ContainerRequestDto> boxes,
-        @NotNull @Valid List<ItemRequestDto> products) {
+        StandardUnitsUtil.Length lengthUnitType,
+        StandardUnitsUtil.Weight weightUnitType,
+        @Valid List<ContainerRequestDto> boxes,
+        @Valid List<ItemRequestDto> products) {
 }
